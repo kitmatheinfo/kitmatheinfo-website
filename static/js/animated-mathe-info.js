d1e6595b81e8f5e2c19c2207cfa6e8438b8d332f
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-06-27 23:27:56 +0200

    Use sliding animation for Mathe/Info

--- static/js/animated-mathe-info.js
+++ static/js/animated-mathe-info.js
@@ -1,38 +1,40 @@
-window.addEventListener("load", () => {
-  const foo = document.querySelector("#animated-mathe-info");
-  if (!foo) {
-    return;
-  }
+function removeNoscriptSettings() {
+  document.querySelector("#animated-mathe-info-spacer").textContent = "Mathe";
+  document.querySelector("#animated-mathe-info-spacer").style.opacity = 0;
+}
 
-  let typed = "";
+window.addEventListener("load", () => {
   let currentBlueprint = 0;
   let blueprints = ["Info", "Mathe"]
-  let reversing = false;
 
-  let max = 0;
-  blueprints.forEach(it => {
-    foo.textContent = it;
-    max = Math.max(max, foo.getBoundingClientRect().width);
-  });
-  foo.style.width = max + "px";
+  removeNoscriptSettings();
 
-  setInterval(() => {
-    if (reversing) {
-      if (typed.length === 0) {
-        reversing = false;
-      } else {
-        typed = typed.substring(0, typed.length - 1);
-      }
-      foo.textContent = typed;
-      return;
-    }
+  const iteration = () => {
+    const currentFoo = document.querySelector(".animated-mathe-info.current");
+    const oldFoo = document.querySelector(".animated-mathe-info.old");
+    const container = document.querySelector(".animated-mathe-info-container");
+    container.style.height = container.parentElement.getBoundingClientRect().height + "px";
 
-    if (typed == blueprints[currentBlueprint]) {
-      currentBlueprint = (currentBlueprint + 1) % blueprints.length;
-      reversing = true;
-      return;
-    }
-    typed = typed + blueprints[currentBlueprint].charAt(typed.length);
-    foo.textContent = typed;
-  }, 200)
+    currentBlueprint = (currentBlueprint + 1) % blueprints.length;
+
+    currentFoo.textContent = blueprints[currentBlueprint];
+    currentFoo.style.transform = "translateY(1em)";
+    currentFoo.style.opacity = 1;
+    currentFoo.classList.add("old");
+    currentFoo.classList.remove("current");
+
+    setTimeout(() => {
+      currentFoo.style.transform = "translateY(0)";
+    }, 1);
+
+    // Outro it!
+    oldFoo.style.transform = "translateY(-1em)";
+    oldFoo.style.opacity = 0;
+    oldFoo.classList.remove("old");
+    oldFoo.classList.add("current");
+  }
+
+  iteration();
+
+  setInterval(iteration, 3000)
 })
